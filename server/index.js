388742497105b20480c1a03378f2af58322f3061
Aditya Srivastava <adityasrvastava2004@gmail.com>
2025-12-21 17:33:14 +0530

    Vercel config file adding

--- server/index.js
+++ server/index.js
@@ -8,24 +8,29 @@ process.on('unhandledRejection', (error) => {
 
 import express from "express"
 import cors from "cors"
-const app=express()
 
+const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended: true}))
+
 app.use(cors({
-    origin:process.env.CORS_ORIGIN ?.split(",") || "http://localhost:5173",
-    credentials:true,
-    methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
-    allowedHeaders:["Authorization","Content-Type"]
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+    allowedHeaders: ["Authorization","Content-Type"]
 }))
 
+
 import videoGeneratingRoute from "./src/route/generateVideo.route.js"
-app.options('*', cors());
-app.use("/api/generate-video-base64",videoGeneratingRoute)
 
-const PORT=8080;
+app.use("/api/generate-video-base64", videoGeneratingRoute)
 
-app.listen(PORT,()=>{
+export default app;
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = 8080;
+  app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
-})
+  })
+}
